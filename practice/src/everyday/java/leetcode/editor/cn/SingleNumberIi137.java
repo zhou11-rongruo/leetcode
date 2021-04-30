d8c3d9e72b7a6b2c943989f0b7bf6327d3413c47
zhou11 <edison.zfb@gmail.com>
2021-04-30 22:11:52 +0800

    2021.04.30

---
+++ practice/src/everyday/java/leetcode/editor/cn/SingleNumberIi137.java
@@ -0,0 +1,65 @@
+//给你一个整数数组 nums ，除某个元素仅出现 一次 外，其余每个元素都恰出现 三次 。请你找出并返回那个只出现了一次的元素。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：nums = [2,2,3,2]
+//输出：3
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：nums = [0,1,0,1,0,1,99]
+//输出：99
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= nums.length <= 3 * 104 
+// -231 <= nums[i] <= 231 - 1 
+// nums 中，除某个元素仅出现 一次 外，其余每个元素都恰出现 三次 
+// 
+//
+// 
+//
+// 进阶：你的算法应该具有线性时间复杂度。 你可以不使用额外空间来实现吗？ 
+// Related Topics 位运算 
+// 👍 647 👎 0
+
+
+package everyday.java.leetcode.editor.cn;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SingleNumberIi137 {
+    public static void main(String[] args) {
+        Solution solution = new SingleNumberIi137().new Solution();
+    }
+        //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int singleNumber(int[] nums) {
+        Map<Integer, Integer> freq = new HashMap<Integer, Integer>();
+        for (int num : nums) {
+            freq.put(num, freq.getOrDefault(num, 0) + 1);
+        }
+        int ans = 0;
+        for (Map.Entry<Integer, Integer> entry : freq.entrySet()) {
+            int num = entry.getKey(), occ = entry.getValue();
+            if (occ == 1) {
+                ans = num;
+                break;
+            }
+        }
+        return ans;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
